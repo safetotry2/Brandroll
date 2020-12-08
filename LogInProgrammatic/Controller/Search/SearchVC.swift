f34df9ae885fedc5acf64db887f678977050d50a
safetotry2 <safetotry2@gmail.com>
2020-12-08 18:02:36 -0500

    Implement search feed and filtering users based on username

--- LogInProgrammatic/Controller/Search/SearchVC.swift
+++ LogInProgrammatic/Controller/Search/SearchVC.swift
@@ -10,11 +10,17 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class SearchVC: UITableViewController {
+class SearchVC: UITableViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     // MARK: - Properties
     
     var users = [User]()
+    var filteredUsers = [User]()
+    var searchBar = UISearchBar()
+    var inSearchMode = false
+    var collectionView: UICollectionView!
+    var collectionViewEnabled = true
+    var posts = [Post]()
 
     private let reuseIdentifier = "SearchUserCell"
     
@@ -27,8 +33,14 @@ class SearchVC: UITableViewController {
         // separator insets
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 64, bottom: 0, right: 0)
         
-        // configure nav controller
-        configureNavController()
+        // configure search bar
+        configureSearchBar()
+        
+        // configure collection view
+        configureCollectionView()
+        
+        // fetch posts
+        fetchPosts()
         
         // fetch users
         fetchUsers()
@@ -45,12 +57,22 @@ class SearchVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        if inSearchMode {
+            return filteredUsers.count
+        } else {
+            return users.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let user = users[indexPath.row]
+        var user: User!
+        
+        if inSearchMode {
+            user = filteredUsers[indexPath.row]
+        } else {
+            user = users[indexPath.row]
+        }
         
         // create instance of user profile vc
         let userProfileVC = UserProfileVC(collectionViewLayout: UICollectionViewFlowLayout())
@@ -65,15 +87,125 @@ class SearchVC: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SearchUserCell
         
-        cell.user = users[indexPath.row]
+        var user: User!
+        
+        if inSearchMode {
+            user = filteredUsers[indexPath.row]
+        } else {
+            user = users[indexPath.row]
+        }
+        
+        cell.user = user
         
         return cell
     }
     
+    // MARK: - UICollectionView
+    
+    func configureCollectionView() {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - (tabBarController?.tabBar.frame.height)! - (navigationController?.navigationBar.frame.height)!)
+        
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.alwaysBounceVertical = true
+        collectionView.backgroundColor = .white
+        
+        tableView.addSubview(collectionView)
+        collectionView.register(SearchPostCell.self, forCellWithReuseIdentifier: "SearchPostCell")
+        
+        tableView.separatorColor = .clear
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 2) / 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchPostCell", for: indexPath) as! SearchPostCell
+        
+        cell.post = posts[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let feedVC = FeedVC(collectionViewLayout: UICollectionViewFlowLayout())
+        
+        feedVC.viewSinglePost = true
+        
+        feedVC.post = posts[indexPath.item]
+        
+        navigationController?.pushViewController(feedVC, animated: true)
+    }
+    
     // MARK: - Handlers
     
-    func configureNavController() {
-        navigationItem.title = "Explore"
+    func configureSearchBar() {
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        searchBar.barTintColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        searchBar.tintColor = .black
+    }
+    
+    // MARK: - UISearchBar
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+        
+        collectionView.isHidden = true
+        collectionViewEnabled = false
+        
+        tableView.separatorColor = .lightGray
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        let searchText = searchText.lowercased()
+        
+        if searchText.isEmpty || searchText == " " {
+            inSearchMode = false
+            tableView.reloadData()
+        } else {
+            inSearchMode = true
+            filteredUsers = users.filter({ (user) -> Bool in
+                return user.username.contains(searchText)
+            })
+            tableView.reloadData()
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        searchBar.showsCancelButton = false
+        inSearchMode = false
+        searchBar.text = nil
+        
+        collectionViewEnabled = true
+        collectionView.isHidden = false
+        
+        tableView.separatorColor = .clear
+        
+        tableView.reloadData()
     }
     
     // MARK: - API
@@ -92,5 +224,18 @@ class SearchVC: UITableViewController {
             }
         }
     }
-
+    
+    func fetchPosts() {
+        
+        posts.removeAll()
+        
+        POSTS_REF.observe(.childAdded) { (snapshot) in
+            let postId = snapshot.key
+            
+            Database.fetchPost(with: postId) { (post) in
+                self.posts.append(post)
+                self.collectionView.reloadData()
+            }
+        }
+    }
 }
---
+++ LogInProgrammatic/View/SearchPostCell.swift
@@ -0,0 +1,43 @@
+//
+//  SearchPostCell.swift
+//  LogInProgrammatic
+//
+//  Created by Mac on 12/8/20.
+//  Copyright © 2020 Eric Park. All rights reserved.
+//
+
+import UIKit
+
+class SearchPostCell: UICollectionViewCell {
+    
+    var post: Post? {
+        
+        didSet {
+            
+            print("Did set post")
+            
+            guard let imageUrl = post?.imageUrl else { return }
+            postImageView.loadImage(with: imageUrl)
+        }
+    }
+    
+    let postImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
