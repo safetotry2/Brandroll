d8450a3f3bb36125a3c813da45c74a7a0df2c53f
Glenn Posadas <hello@glennvon.com>
2021-02-20 18:32:35 +0800

    BRD1.5 - start root flow.

---
+++ LogInProgrammatic/Controller/Base/BaseVC.swift
@@ -0,0 +1,11 @@
+//
+//  BaseVC.swift
+//  LogInProgrammatic
+//
+//  Created by Glenn Posadas on 2/20/21.
+//  Copyright © 2021 Eric Park. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController { }
---
+++ LogInProgrammatic/Controller/Root/RootVC.swift
@@ -0,0 +1,23 @@
+//
+//  RootVC.swift
+//  LogInProgrammatic
+//
+//  Created by Glenn Posadas on 2/20/21.
+//  Copyright © 2021 Eric Park. All rights reserved.
+//
+
+import Firebase
+import UIKit
+
+class RootVC: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Overrides
+    // MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
