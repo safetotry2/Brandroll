b4011e682997373075a019bd152ff4fddc7745d4
Glenn Posadas <hello@glennvon.com>
2021-02-21 19:25:49 +0800

    BRD1.5 - fix messagesVC removing observers by storing handles to array. 🔥🥳

--- LogInProgrammatic/Model/MessagesUtils.swift
+++ LogInProgrammatic/Model/MessagesUtils.swift
@@ -17,66 +17,83 @@ class MessagesUtils: NSObject {
     
     var lastFetchedMessage: Message?
     
-    /// The handle of the Firebase observer from `fetchMessages`.
-    private var messagesRefHandle: DatabaseHandle?
-    /// The uid passed into `messagesRefHandle`.
-    private var uidUser: String?
+    /// The handle of the Firebase observer from `fetchMessages`. The uid is the one that is passed into `messagesRefHandle`.
+    typealias firstTuple = (dbHandle: DatabaseHandle?, uidUser: String?)
+    private var messagesRefHandles = [firstTuple]()
     
-    /// Second handle.
-    private var userMessagesRefHandle: DatabaseHandle?
-    /// The uid passed into `userMessagesRefHandle`.
-    private var uidMessage: String?
+    /// The second handle. The uid is passed into `userMessagesRefHandle`.
+    typealias secondTuple = (dbHandle: DatabaseHandle?, uidUser: String?, uidMessage: String?)
+    private var userMessagesRefHandles = [secondTuple]()
     
     // MARK: Functions
     
     func removeObserver() {
-        // The first observer.
-        if let handle = messagesRefHandle,
-           let uidUser = uidUser {
-            
-            USER_MESSAGES_REF
-                .child(uidUser)
-                .removeObserver(withHandle: handle)
+        // The first observers.
+        messagesRefHandles.forEach { (dbHandle, uidUser) in
+            if let handle = dbHandle,
+               let uidUser = uidUser {
+                USER_MESSAGES_REF
+                    .child(uidUser)
+                    .removeObserver(withHandle: handle)
+            }
         }
         
         // The second observer
-        if let handle = userMessagesRefHandle,
-           let uidUser = uidUser,
-           let uidMessage = uidMessage {
-            USER_MESSAGES_REF
-                .child(uidUser)
-                .child(uidMessage)
-                .removeObserver(withHandle: handle)
+        userMessagesRefHandles.forEach { (dbHandle, uidUser, uidMessage) in
+            if let handle = dbHandle,
+               let uidUser = uidUser,
+               let uidMessage = uidMessage {
+                USER_MESSAGES_REF
+                    .child(uidUser)
+                    .child(uidMessage)
+                    .removeObserver(withHandle: handle)
+            }
         }
     }
     
     func fetchMessages(userId: String, completion block: FetchMessageCompletion?) {
-        uidUser = userId
+        var newMessagesRefHandle: firstTuple = (nil, nil)
         
-        messagesRefHandle = USER_MESSAGES_REF
+        newMessagesRefHandle.uidUser = userId
+        
+        let newHandle = USER_MESSAGES_REF
             .child(userId)
             .observe(.childAdded, with: { [weak self] (snapshot) in
-                let uid = snapshot.key
-                self?.uidMessage = uid
+                let messageId = snapshot.key
                 
                 self?.continueFetchingMessage(
                     userId: userId,
-                    messageId: uid,
+                    messageId: messageId,
                     completion: block
                 )
             }, withCancel: { (error) in
                 if let block = block { block?("") }
             })
+        
+        newMessagesRefHandle.dbHandle = newHandle
+        
+        // store to handles array.
+        messagesRefHandles.append(newMessagesRefHandle)
     }
     
     func continueFetchingMessage(userId: String, messageId: String, completion block: FetchMessageCompletion?) {
-        userMessagesRefHandle = USER_MESSAGES_REF
+        var newUserMessagesRefHandle: secondTuple = (nil, nil, nil)
+        
+        newUserMessagesRefHandle.uidUser = userId
+        newUserMessagesRefHandle.uidMessage = messageId
+        
+        let newHandle = USER_MESSAGES_REF
             .child(userId)
             .child(messageId)
             .observe(.childAdded) { [weak self] (snapshot) in
                 let messageId = snapshot.key
                 self?.fetchMessage(withMessageId: messageId, complection: block)
             }
+        
+        newUserMessagesRefHandle.dbHandle = newHandle
+        
+        // store to handles array.
+        userMessagesRefHandles.append(newUserMessagesRefHandle)
     }
     
     func fetchMessage(withMessageId messageId: String, complection block: FetchMessageCompletion?) {
