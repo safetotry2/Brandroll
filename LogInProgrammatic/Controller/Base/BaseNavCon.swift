ff4fe00d20f1e5809e4c0cf306592906eee6ba35
Glenn Posadas <hello@glennvon.com>
2021-03-26 23:44:43 +0800

    BRD3 - update BaseNavCon doc.

--- LogInProgrammatic/Controller/Base/BaseNavCon.swift
+++ LogInProgrammatic/Controller/Base/BaseNavCon.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 /**
- This base navigationController class conforms to `StatusBarToggelable` protocol.
+ This base navigationController class also conforms to `StatusBarToggelable` protocol.
  We can set the statusBar states (ie visibility, and animation style) by using navCon init method.
  We can also set it by using the extension method `setStatusBarAppearance` of `UIViewController`.
  The reason why we put everything in the navCon subclass is that ideally and most of the time, we embed the controller
- in a navCon even if our controller has no navBar.
+ in a navCon even if our controller has no navBar. This means if we have a standalone modal, we could embed it in a navController to use this `StatusBarToggleable`.
  */
 class BaseNavCon: UINavigationController, StatusBarToggleable {
     
